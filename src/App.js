fb2a1455800a4f6ef309087b369dde3de1e0c13d
mipunaruwan <nipunakalpa1234@gmail.com>
2022-08-07 13:58:30 +0530

    login page setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import LoginForm from './Page/login/login';
+
+
+const App=() =>{
+  return (
+   <div>
+<LoginForm/>
+
+   </div>
+
+  );
+}
+
+export default App;
---
+++ src/Page/login/login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './login.css'
+const LoginForm = () => {
+  return (
+    <div className='main_panal'> 
+
+<div className='login_panal'>
+<div id='box_lable'>
+<div id='icon_image'></div>
+<h3>Login</h3>
+</div>
+</div>
+
+<div id='boxform'>
+  <input type="text" placeholder='Username'/>
+  <input type="password" placeholder='password'/>
+
+</div>
+<div id='box_botton'>
+
+<div id='btn_login'><a href="/login" id='a_login'>Login</a></div>
+ <h5>Create new user account? <a href="/singup">click here</a></h5>
+</div>
+
+<div id=''></div>
+<h3>Login</h3>
+
+
+
+    </div>
+  )
+   
+}
+
+export default LoginForm
