5e37e0b06f5815e26baec2f1a2a91024fdd5a0f6
mipunaruwan <nipunakalpa1234@gmail.com>
2022-09-03 21:07:53 +0530

    sessio file are updated and navigation is worked

--- src/Page/Session/notfound.js
+++ src/Page/Session/notfound.js
@@ -2,12 +2,20 @@ import React from 'react'
 import './NotFound.css'
 import notFound from './assets/404.jpg'
 import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button';
 function NotFound() {
     return (
-        <div>
-            <img src={notFound} alt=""></img>
+        <div className='wrong__Container'>
+        <div className='image__Container'>
+            <img src={notFound} alt='404 error' className='image' />
         </div>
-    )
+        <Link to="/">
+            <Button variant="outlined" size="medium">
+                Go To Back Page
+            </Button>
+        </Link>
+    </div>
+    );
 }
 
 export default NotFound 
